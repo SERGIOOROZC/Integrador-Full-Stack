cac23955956852ab6f12b7ed0ff92b29bfbda8a8
Sergio Orozco <sforozco1974@gmail.com>
2025-09-15 14:23:25 -0300

    (frontend): Creación de estructura de carpetas y componentes iniciales

---
+++ cliente/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Inicia sesión</h1>
+      <p>Aquí irán los campos para que el usuario ingrese su información.</p>
+    </div>
+  );
+};
+
+export default Login;
